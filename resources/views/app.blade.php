90d79da869724985bc3fb3f323f80e46faf6a5a0
saifalbd <saifurbd89@gmail.com>
2023-03-01 23:31:25 +0600

    fixBuild

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,14 +10,19 @@
     rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
   />
+  <link rel=”manifest” href="/build/manifest.json">
 	@routes
-	@vite('resources/css/app.scss')
+	
+	 @vite('resources/css/app.scss')
+	
 </head>
 
 <body>
-	<div id="app"></div>
 
+	<div id="app"></div>
 	@vite('resources/js/app.js')
+	
+	
 </body>
 
 </html>
